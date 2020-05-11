c3bc306906f88c46ae14af3a134390597e2ff314
giat215 <63117745+giat215@users.noreply.github.com>
2020-05-11 19:49:49 +0700

    Update sample_config.py

--- IHbot/sample_config.py
+++ IHbot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1200238538:AAG2nfy8I2p66n9AvxYm013G-m8044dlBao"
+    OWNER_ID = "1127975045"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "michael2156"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
